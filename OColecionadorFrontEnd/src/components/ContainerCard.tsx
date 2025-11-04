0356a4d34d67be7d3dfa8f17c0f6f91397ec7166
patrickcaloriocarvalho <patrick.calorio@hotmail.com>
2025-11-04 11:48:15 -0300

    lucide-react

--- OColecionadorFrontEnd/src/components/ContainerCard.tsx
+++ OColecionadorFrontEnd/src/components/ContainerCard.tsx
@@ -1,5 +1,6 @@
-import { FaPlay, FaStop, FaRedo } from 'react-icons/fa';
+
 import { Container } from '../models/Docker';
+import { Play, StopCircle, RefreshCw } from 'lucide-react';
 import './ContainerCard.css';
 
 interface Props {
@@ -8,7 +9,7 @@ interface Props {
 }
 
 export function ContainerCard({ container, onAction }: Props) {
-  
+
   type ContainerState = 'running' | 'exited' | 'created';
 
   const statusColor: Record<ContainerState, string> = {
@@ -25,22 +26,24 @@ export function ContainerCard({ container, onAction }: Props) {
       <p><strong>Imagem:</strong> {container.image}</p>
       <p><strong>Comando:</strong> {container.command}</p>
       <p>
+
+
         <strong>Status:</strong>{' '}
         <span style={{ color, fontWeight: 'bold' }}>{container.state}</span>
       </p>
       <div className="actions">
         {container.state !== 'running' && (
           <button onClick={() => onAction('start', container.id)} title="Iniciar">
-            <FaPlay />
+            <Play />
           </button>
         )}
         {container.state === 'running' && (
           <button onClick={() => onAction('stop', container.id)} title="Parar">
-            <FaStop />
+            <StopCircle />
           </button>
         )}
         <button onClick={() => onAction('restart', container.id)} title="Reiniciar">
-          <FaRedo />
+          <RefreshCw />
         </button>
       </div>
     </div>
