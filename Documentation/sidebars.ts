dd79f5bcff06232a921ffe16a1cedb7c5fe8153c
patrickcaloriocarvalho <patrick.calorio@hotmail.com>
2025-11-11 23:31:01 -0300

    Configuração GitHub Pages

---
+++ Documentation/sidebars.ts
@@ -0,0 +1,14 @@
+import type { SidebarsConfig } from '@docusaurus/plugin-content-docs';
+
+const sidebars: SidebarsConfig = {
+  docs: [
+    'intro',
+    'architecture',
+    'getting-started',
+    'api',
+    'ia',
+    'contribution',
+  ],
+};
+
+export default sidebars;
