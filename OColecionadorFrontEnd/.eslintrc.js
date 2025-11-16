055afebb68d91f16b13b593b369b394bae434f0a
patrickcaloriocarvalho <patrick.calorio@hotmail.com>
2025-11-16 20:47:26 -0300

    ReactAndReactNative

---
+++ OColecionadorFrontEnd/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  settings: { react: { version: 'detect' } },
+  env: { browser: true, node: true, es6: true, jest: true },
+  rules: {}
+};
---
+++ OColecionadorMobile/.eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'react', 'react-hooks'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier'
+  ],
+  settings: { react: { version: 'detect' } },
+  env: { node: true, jest: true, 'react-native/react-native': true },
+  rules: {}
+};
